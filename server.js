e33e92549bab4f121e40985a381ee0fdb89b8900
Ali Hazer <alihazerr28@gmail.com>
2024-10-15 12:29:38 +0300

    Test

--- server.js
+++ server.js
@@ -1,10 +1,8 @@
 const express = require('express');
-const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const dotEnv = require('dotenv');
 const {dbConnect} = require('./config/dbConnect.js')
 const cookieParser = require('cookie-parser');
-const serviceAccount = require('./config/serviceAccountKey.json');
 const admin = require('firebase-admin');
 const path = require('path');
 const cors = require('cors');
@@ -38,11 +36,24 @@ app.use('/api/token', require('./routes/tokenRoutes.js'));
 app.use(require('./routes/ejsRoutes/ejs.routes'));
 app.use('/images', express.static('images'));
 
-
+const serviceAccount = {
+    type: process.env.GOOGLE_PRIVATE_KEY_TYPE || "service_account",
+    project_id: process.env.GOOGLE_PROJECT_ID,
+    private_key_id: process.env.GOOGLE_PRIVATE_KEY_ID,
+    private_key: process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+    client_email: process.env.GOOGLE_CLIENT_EMAIL,
+    client_id: process.env.GOOGLE_CLIENT_ID,
+    auth_uri: process.env.GOOGLE_AUTH_URI,
+    token_uri: process.env.GOOGLE_TOKEN_URI,
+    auth_provider_x509_cert_url: process.env.GOOGLE_AUTH_PROVIDER_CERT_URL,
+    client_x509_cert_url: process.env.GOOGLE_CLIENT_CERT_URL,
+    universe_domain: process.env.GOOGLE_UNIVERSE_DOMAIN
+};
 
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
 });
+
 
 
 
